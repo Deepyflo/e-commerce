b2f02a9c6f16ca248cec221134017f2b20d073fe
Florian Renders <florianrenders@gmail.com>
2022-01-21 12:01:07 +0100

    burger menu work without exactly fine style

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const toggleMenu = () => {  
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', (e) => {    
+        navbar.classList.toggle('show-nav');
+    });    
+}
+toggleMenu();
